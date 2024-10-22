8f7138581bf13a8b0557ed4918dc9ff7ec065b29
swingcat <swimmingcat2021@163.com>
2024-10-23 01:46:36 +0800

    [Feature] Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,12 @@
+#include "Application.h"
+
+namespace Hazel {
+
+	Application::Application() = default;
+
+	Application::~Application() = default;
+
+	void Application::Run() {
+		while (true);
+	}
+}
